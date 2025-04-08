867f1b8c5c2a24a6cda54fd15b7e76d7bda93791
Burhanuddin-Kachwala <burhanuddin.ast@gmail.com>
2025-04-08 12:07:11 +0530

    rendered cards using map( )

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,33 @@ import './App.css'
 import { useState } from 'react'
 import Card from "./Components/Card.jsx";
 
-
+let jobs = [
+  {
+    id: 1,
+    title: "Internship",
+    description: "This internship offers great opprtunity to young students who have a zest in laravel",
+  },
+  {
+    id: 2,
+    title: "Apprenticeship",
+    description: "This Apprenticeship offers great opprtunity to young adults who have a zest in MERN stack",
+  },
+  {
+    id: 3,
+    title: "Fellowship",
+    description: "This Fellowship offers great opprtunity to young adults who have a zest in AI/ML",
+  },
+  {
+    id: 4,
+    title: "Scholarship",
+    description: "This Scholarship offers great opprtunity to young adults who have a zest in Data Science",
+  },
+  {
+    id: 5,
+    title:"Employment",
+    description: "This Employment offers great opprtunity to young adults who have a zest in Software Development",
+  },
+];
 
 function App() {
   let [counter,setCounter] = useState(0);// default value of counter is 0
@@ -23,14 +49,14 @@ function App() {
       <h3>Counter Value : {counter}</h3>
       <button onClick={addValue}>Increment</button>&nbsp;
       <button onClick={decreaseValue}>Decrement</button>
-      <Card
-        title="Internship"
-        description="This internship offers great opprtunity to young students who have a zest in laravel"
-      />
-      <Card
-        title="Apprenticeship"
-        description="This Apprenticeship offers great opprtunity to young adults who have a zest in MERN stack"
-      />
+    <div className='flex flex-wrap justify-center gap-3'>
+      {jobs.map(job => (
+        <Card         
+          title={job.title}
+          description={job.description}
+        />
+      ))}
+      </div>
     </>
   );
 }
